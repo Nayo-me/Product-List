4a8a5b129418b07f957de43e0953fe18ee1067b7
Nayo-me <anushikanayomi2@gmail.com>
2024-08-06 13:52:12 +0530

    implementing product service file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/componentfolder/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient() // Ensure HttpClientModule is provided
+  ]
+})
+  .catch(err => console.error(err));
