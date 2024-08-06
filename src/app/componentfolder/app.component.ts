4d300e649c49002c4d6df56c920e785471f9ddb7
Nayo-me <anushikanayomi2@gmail.com>
2024-08-06 13:35:10 +0530

    making changes to root component files and it's path

--- src/app/componentfolder/app.component.ts
+++ src/app/componentfolder/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ProductsComponent } from './products/products.component';
+import { AddProductComponent } from "./add-product/add-product.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ProductsComponent, AddProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
